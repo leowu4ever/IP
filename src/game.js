9c71c5032328a3baa2de0e0bbb026537fb2f318b
leo <leowu507@gmail.com>
2016-07-08 10:29:14 +0100

    spawn map and item properly now

--- src/game.js
+++ src/game.js
@@ -1,30 +1,57 @@
+var width = window.innerWidth // 981
+var height = window.innerHeight // 553
+
+var platforms = ["floating", "wide", "normal", "tall"]
+var items = ["diamond", "enemy", "player", "star"]
+
+var wave_y = 0.8 * height
+
+var platform_normal_x = 0 * width
+var platform_normal_y = 0.6 * height
+
+var platform_floating_x = 0.2 * width
+var platform_floating_y = 0.2 * height
+
+var platform_high_x = 0.45 * width
+var platform_high_y = 0.3 * height
+
+var platform_wide_x = 0.75 * width
+var platform_wide_y = 0.65 * height
+
 Game = {
   start: function() {
-    var width = window.innerWidth
-    var height = window.innerHeight
+
     Crafty.init(width, height)
-    // platform 
 
-    // decoration
-
-    // character 
-
-    Crafty.e("2D, DOM, Image").attr({x: 0, y: 500}).image("assets/wave.png")
-    Crafty.e("2D, DOM, Image").attr({x: 0, y: 450}).image("assets/platform_normal.png")
-    Crafty.e("2D, DOM, Image").attr({x: 200, y: 150}).image("assets/platform_high.png")
-    Crafty.e("2D, DOM, Image").attr({x: 500, y: 300}).image("assets/platform_float.png")
-    Crafty.e("2D, DOM, Image").attr({x: 700, y: 350}).image("assets/platform_wide.png")
-    Crafty.e("2D, DOM, Image").attr({x: 300, y: 120}).image("assets/character.png")
-    Crafty.e("2D, DOM, Image").attr({x: 120, y: 410}).image("assets/character_2.png")
-    Crafty.e("2D, DOM, Image").attr({x: 50, y: 420}).image("assets/diamond.png")
-    Crafty.e("2D, DOM, Image").attr({x: 850, y: 320}).image("assets/wood.png")
-    Crafty.e("2D, DOM, Image").attr({x: 850, y: 250}).image("assets/cloud.png")
-    Crafty.e("2D, DOM, Image").attr({x: 530, y: 270}).image("assets/star.png")
-    Crafty.e("2D, DOM, Image").attr({x: 600, y: 270}).image("assets/star.png")
+    // dynamic map generation 
+    // iphone 6 dependent 
+    // platform  1. normal 2. floating 3. wide  4. tall narrow
+    // task 1. diamond award 2. enemy 3. character 2 question 4. counting stars
     
-    Crafty.e("2D, DOM, Image").attr({x: 770, y: 317}).image("assets/grass.png")
+    // wave first
+    spawn (0, wave_y, "wave")
+    // for each platform from left to right
+    // normal
+    spawn (platform_normal_x, platform_normal_y, "platform_normal")
+    spawn (platform_normal_x + 70, platform_normal_y - 30, "star")
 
+    // floating
+    spawn (platform_floating_x, platform_floating_y, "platform_floating")
+    spawn (platform_floating_x + 40, platform_floating_y - 30, "diamond")
+
+    // high
+    spawn (platform_high_x, platform_high_y, "platform_high")
+    spawn (platform_high_x + 20, platform_high_y - 30, "player")
+
+    // wide + wood + grass + character_2 + cloud
+    spawn (platform_wide_x, platform_wide_y, "platform_wide")
+    spawn (platform_wide_x + 40, platform_wide_y - 34, "character_2")
+    spawn (platform_wide_x + 100, platform_wide_y - 34, "grass")
+    spawn (platform_wide_x + 170, platform_wide_y - 30, "wood")
+    spawn (platform_wide_x + 170, platform_wide_y - 80, "cloud")
   }
 }
+
+function spawn(_x, _y, name) { return Crafty.e("2D, DOM, Image").attr({x: _x, y: _y}).image("assets/" + name + ".png") }
 
 
