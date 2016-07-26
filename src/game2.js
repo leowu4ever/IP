f4320477063865b93252d0fd5ac049b13efdc807
leo <leowu507@gmail.com>
2016-07-26 08:52:30 +0100

    added more sprite to game3

--- src/game2.js
+++ src/game2.js
@@ -1,49 +1,22 @@
 var width = window.innerWidth // 981
 var height = window.innerHeight // 553
 
-// game 1
-var wave_y = 0.8 * height
-
-var platform_normal_x = 0 * width
-var platform_normal_y = 0.6 * height
-
-var platform_floating_x = 0.2 * width
-var platform_floating_y = 0.2 * height
-
-var platform_high_x = 0.45 * width
-var platform_high_y = 0.3 * height
-
-var platform_wide_x = 0.75 * width
-var platform_wide_y = 0.65 * height
-
-var wave
-var platform_normal, platform_floating, platform_high, platform_wide
-var player
-var pos = 2
-var platforms = []
-var numbers = []
 var recordingImg
+var a1, a2, a3, a4
+var f1, f2, answer
 
 // game 2
 var wheel
 
+var correct, tryagain
+
 Game2 = {
-  
 
   showNumber: function (index) {
     var content
-    if (index == 10) { content = "left" }
-    else if (index == 11) { content = "right" }
-    else if (index == 12) { content = "square" }
-    else if (index == 13) { content = "triangle" }
-    else { content = index.toString() }
-
-    var num = spawn(width - 200, 0, content)
-    num.timeout(function () {
-      num.destroy();
-    }, 2000);
+    var num = spawn(width - 200, 0, content.toString())
+    num.timeout(function () { num.destroy() }, 2000);
   },
-
 
   showRecordingImg: function () {
     recordingImg.tween({ alpha: 1.0 }, 200)
@@ -53,22 +26,47 @@ Game2 = {
     recordingImg.tween({ alpha: 0.0 }, 200)
   },
 
-
   // game 2
   gamestart: function () {
     Crafty.init(width, height)
-    wheel = spawn(300, 50, "circle_base")
-    wheel.origin("center")
-    spawn(370, 70, "1")
-    spawn(520, 70, "2")
-    spawn(370, 270, "3")
-    spawn(520, 270, "4")
+    initQuestion()
+
+    wheel = spawn(50, 50, "circle_base")
+    spawn(140, 110, "1")
+    spawn(290, 110, "2")
+    spawn(140, 280, "3")
+    spawn(290, 280, "4")
+    
+    a1 = spawn(50, 10, "a1")
+    //a1.attr({ alpha: 0.0 })
+    a2 = spawn(370, 10, "a2")
+    a2.attr({ alpha: 0.0 })
+    a3 = spawn(50, 400, "a3")
+    a3.attr({ alpha: 0.0 })
+    a4 = spawn(370, 400, "a4")
+    a4.attr({ alpha: 0.0 })
+
+    spawn(500, 100, f1.toString() + "_")
+    spawn(580, 100, "minus")
+    spawn(660, 100, f2.toString() + "_copy")
+    spawn(740, 100, "equal")
+
     recordingImg = spawn(width / 3 + 60, height / 2.5, "recording")
     recordingImg.attr({ alpha: 0.0 })
-  }
+  },
 
 }
 
+function getRandomInt(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min }
+
 function spawn(x, y, name) { return Crafty.e("2D, DOM, Image, Tween").attr({ x: x, y: y }).image("assets/game2/" + name + ".png") }
 
+function initQuestion() {
+  f1 = getRandomInt(0, 9)
+  answer = getRandomInt(0, f1)
+  f2 = f1 - answer
+
+  hyper.log(f1)
+  hyper.log(f2)
+}
 
