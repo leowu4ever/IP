f45f90811421eef341ea09fecb68775f490b6ecc
leo <leowu507@gmail.com>
2016-08-03 13:23:06 +0100

    use training data to test

--- src/gesture.js
+++ src/gesture.js
@@ -146,7 +146,7 @@ function assemblyReading(x, y, z) {
   return temp
 }
 
-function getDistance(v1, v2) { return Math.sqrt(Math.pow((v1[0] - v2[0]), 2) + Math.pow((v1[1] - v2[1]), 2) + Math.pow((v1[2] - v2[2]), 2)) }
+function getDistance(v1, v2) { return Math.sqrt(Math.pow((v1[0] - v2[0]), 2) + Math.pow((v1[1] - v2[1]), 2)) }
 
 
 function minIndex(arr) {
